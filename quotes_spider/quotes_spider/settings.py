2b0839fdc87645ef265f31146d35717ac5f2ddbf
Eduardo Henrique Kenji Shibukawa <EduardoShibuka@gmail.com>
2017-09-09 15:38:11 -0300

    Adicionado agente para o scrapper

--- quotes_spider/quotes_spider/settings.py
+++ quotes_spider/quotes_spider/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'quotes_spider.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'quotes_spider (+http://www.yourdomain.com)'
-
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
